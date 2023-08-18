f41bf168d526be7bfd648f9c4f5d236101b9bd40
lowern1ght <lowern1ght@yahoo.com>
2023-08-19 03:12:10 +0500

    fetch: clear sql query

--- SQLSolution/solution.sql
+++ SQLSolution/solution.sql
@@ -1,30 +1,4 @@
-CREATE TABLE category (
-    id int primary key not null,
-    name varchar(255) not null
-);
-
-CREATE TABLE product (
-     id int primary key not null,
-     category_id int foreign key REFERENCES category(id),
-     name varchar(255) not null,
-);
-
-INSERT INTO category 
-VALUES 
-    (1, 'milk'),
-    (2, 'shoes'),
-    (3, 'bags'),
-    (4, 'lamp');
-
-INSERT INTO product
-VALUES 
-    (1, 1, 'dobrii molochnik'),
-    (2, 3, 'nike bag'),
-    (3, 3, 'adidas bag'),
-    (4, null, 'uygyrt'),
-    (5, null, 'yslyga');
-
-SELECT 
+SELECT
     product.name AS 'name',
     c.name AS 'category'
 FROM product
