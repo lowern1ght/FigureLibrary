9f18d9c9ed9da0b3e92da05de85583670e91e60c
lowern1ght <lowern1ght@yahoo.com>
2023-08-19 03:27:23 +0500

    fetch: clear

--- SQLSolution/solution.sql
+++ SQLSolution/solution.sql
@@ -4,12 +4,3 @@ SELECT
 FROM product
 LEFT JOIN category AS c
     ON product.category_id = c.id
-
-/* На текущий момент нет доступа к ТЗ, поэтому я добавлю ветку к запросу для вывода пустых категорий*/
-
-SELECT
-    product.name AS 'name',
-    c.name AS 'category'
-FROM product
-FULL JOIN category AS c
-    ON product.category_id = c.id
