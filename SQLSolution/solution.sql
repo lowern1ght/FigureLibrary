31ea22929a8840f19e901c0fe8a8d4439c1a0871
lowern1ght <lowern1ght@yahoo.com>
2023-08-19 20:36:38 +0500

    fetch: ready query

--- SQLSolution/solution.sql
+++ SQLSolution/solution.sql
@@ -1,16 +1,27 @@
-/*
-create table category (
-    id int identity primary key,
-    name varchar(100)
+/* many-to-many */
+
+create table product_o (
+    id int identity primary key not null,
+    name varchar(100) not null
 );
 
-create table product (
-    id int identity primary key,
-    name varchar(100),
-    category_id int references category
+create table category_o (
+    id int identity primary key not null,
+    title varchar(100)
 );
-*/
 
-select product.[name] as 'Продукт', c.[name] as 'Категория'
-from product
-left join category c on c.id = product.category_id
+create table product_category (
+    product_id int references product_o,
+    category_id int references category_o
+);
+
+insert into category_o values ('sport'), ('food')
+
+insert into product_o values ('ball'), ('cheese'), ('tomato'), ('shirt'), ('smoking')
+
+insert into product_category values (1, 1), (2, 2), (3, 2)
+
+select pr.name, c.title
+from product_o as pr
+    left join product_category pc on pr.id = pc.product_id
+    left join category_o c on pc.category_id = c.id
