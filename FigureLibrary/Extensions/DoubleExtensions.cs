81a32f6ab93a8ac6b2dbbcfc2417c59be0053151
lowern1ght <lowern1ght@yahoo.com>
2023-11-03 02:15:37 +0500

    [feature]: add helper with Tolerance value and extensions to double

---
+++ FigureLibrary/Extensions/DoubleExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace FigureLibrary.Extensions;
+
+public static class DoubleExtensions
+{
+    public static bool IsNull(this double d)
+    {
+        return d == 0.0;
+    }
+
+    public static bool IsLessNull(this double d)
+    {
+        return d <= 0.0;
+    }
+}
