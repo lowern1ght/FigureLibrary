f0cc25879289b1f40933c3573e07349cbf632452
lowern1ght <lowern1ght@yahoo.com>
2023-11-03 02:14:30 +0500

    [feature]: add custom exception

---
+++ FigureLibrary/Exceptions/SideInitializationException.cs
@@ -0,0 +1,9 @@
+﻿namespace FigureLibrary.Exceptions;
+
+public class SideInitializationException : Exception
+{
+    public SideInitializationException(double side)
+        : base($"Error init side: {side}")
+    {
+    }
+}
